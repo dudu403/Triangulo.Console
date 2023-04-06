833a3facbf3eed2007636f5fd751c7c449f4a9f1
IFMathIFSCLages <114521300+IFMathIFSCLages@users.noreply.github.com>
2023-04-06 14:46:16 -0300

    Add project files.

---
+++ Triangulo.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Triangulo.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TipoDeTriangulo triangulo = new TipoDeTriangulo();
+            triangulo.x = 10;
+            triangulo.y = 50;
+            triangulo.z = 50;
+
+            Console.WriteLine("O triângulo é {0}", triangulo.CalcularTipoDeTriangulo());
+        }
+    }
+}
---
+++ Triangulo.ConsoleApp/TipoDeTriangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo.ConsoleApp
+{
+    internal class TipoDeTriangulo
+    {
+        public double x;
+        public double y;
+        public double z;
+
+        public string CalcularTipoDeTriangulo()
+        {
+            if (x < y + z && y < x + z && z < x + y)
+            {
+                return "Equilátero";
+            }
+            else if (x == y || y == z || y == x)
+            {
+                return "Isósceles";
+            }
+            else
+            {
+                return "Escaleno";
+
+            }
+        }
+    } 
+
+}
